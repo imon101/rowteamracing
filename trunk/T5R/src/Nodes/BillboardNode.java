466f8f096eb45e3d660db3dee52ad4d248f0523b
Cid.Lithium <Cid.Lithium@4aab709d-1ea4-e3af-003a-e53461776f31>
2010-12-07 05:03:11 +0000

    Billboards and skybox

--- trunk/T5R/src/Nodes/BillboardNode.java
+++ trunk/T5R/src/Nodes/BillboardNode.java
@@ -4,29 +4,51 @@
  */
 
 package Nodes;
+import com.jme3.asset.AssetManager;
+import com.jme3.material.Material;
+import com.jme3.material.RenderState.BlendMode;
+import com.jme3.material.RenderState.FaceCullMode;
 import com.jme3.math.Matrix3f;
+import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.Camera;
+import com.jme3.renderer.queue.RenderQueue;
+import com.jme3.renderer.queue.RenderQueue.ShadowMode;
+import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
+import com.jme3.scene.shape.Box;
 
 /**
  *
  * @author ArkanRow
  */
 public class BillboardNode extends Node {
-
+    final float billboardThickness =  0.001f;
     private Camera camera;
 
     private Matrix3f orientation;
     private Vector3f look;
     private Vector3f left;
 
-    public BillboardNode(Camera camera) {
+    public BillboardNode(Camera camera, AssetManager assetManager, String material, Vector2f size) {
         this.camera = camera;
 
         orientation = new Matrix3f();
         look = new Vector3f();
         left = new Vector3f();
+
+        Box plane = new Box(Vector3f.ZERO, size.x, -size.y, billboardThickness);
+        Geometry geom = new Geometry("Billboard", plane);
+        geom.setShadowMode(ShadowMode.Off);
+        Material mat = assetManager.loadMaterial(material);
+        mat.getAdditionalRenderState().setBlendMode(BlendMode.Alpha);
+        mat.getAdditionalRenderState().setFaceCullMode(FaceCullMode.Off); // show back side too
+        mat.getAdditionalRenderState().setAlphaTest(true); // alpha on each face
+        geom.setQueueBucket(RenderQueue.Bucket.Transparent); // IMPORTANT
+
+
+        geom.setMaterial(mat);
+        attachChild(geom);
     }
 
     @Override
